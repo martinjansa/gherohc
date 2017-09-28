f99998697f3a6159fee0523045b6d734b577316d
Martin Jansa <martin.jansa@seznam.cz>
2017-09-28 22:34:08 +0200

    added interfaces for light

---
+++ homekitconnector.go
@@ -0,0 +1,11 @@
+package main
+
+// HomeKitConnector represents an interface for definishion and publishing of the accessories to the Apple HomeKit using the HAP protocol
+type HomeKitConnector interface {
+
+	// AddLightAccessory creates a new accessory of type light
+	AddLightAccessory(name string, manufacturer string, serialnumber string, model string, lightmanipulator *HWLightManipulator) error
+
+	// PublishAccessories starts the communication via the HAP protocol
+	PublishAccessories(pin string) error
+}
---
+++ hwlightmanipulator.go
@@ -0,0 +1,11 @@
+package main
+
+// HWLightManipulator represents a HW Light
+type HWLightManipulator interface {
+
+	// returns the current light status ()
+	//IsTurnedOn() error, bool
+
+	// TunLight commands the light to turn on or off
+	TunLight(on bool) error
+}
